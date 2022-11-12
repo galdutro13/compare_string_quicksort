3855d9fe0f16f22319ad6fe663b569c335c8ca05
Gregório A. Almeida Filho <gregassagraf@usp.br>
2022-11-12 15:03:28 -0300

    continue implementation of scratch file

--- tmp_algo.c
+++ tmp_algo.c
@@ -3,23 +3,38 @@
 //
 
 
+int __attribute__((hot, flatten)) strcmp(const char *X, const char *Y)
+{
 
+}
+
+unsigned int inline find_end(char const* arr, int length)
+{
+    unsigned int index = 1;
+
+    while(arr[length - index] != ' ')
+        index++;
+
+    return index-1;
+}
 void quicksort(char const* arr, unsigned int length)
 {
     unsigned int i = 0;
     unsigned int piv = 0;
-
+    unsigned int startofend = 0;
     if(length <= 1)
         return;
 
+    startofend = find_end(arr, length);
+
     do {
 
-        if(arr[i] == ' ')
+        if(arr[i] == ' ' || arr[i] == '\n')
         {
             i++;
             continue;
         }
-        if(strcmp(arr[i], arr[length - 1]) < 0)
+        if(strcmp(&arr[i], &arr[length - startofend]) < 0)
             swap(arr + i, arr + length -1);
 
         i++;
